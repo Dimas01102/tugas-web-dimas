754dbe7843007a55c6f0a33243a2efc3acab2385
Dimas01102 <ddimasddpprasetiyo@gmail.com>
2025-01-15 13:27:05 +0700

    Add files via upload

---
+++ Dimas_form_7/script.js
@@ -0,0 +1,16 @@
+
+const togglePassword = document.querySelector('#toggle-password');
+const password = document.querySelector('#password');
+
+password.type = 'password'; 
+togglePassword.src = 'asset/view.png'; 
+
+togglePassword.addEventListener('click', function () {
+
+    const type = password.type === 'password' ? 'text' : 'password';
+    password.type = type;
+
+    
+    const newSrc = password.type === 'password' ? 'asset/view.png' : 'asset/hide.png';
+    togglePassword.src = newSrc;
+});
